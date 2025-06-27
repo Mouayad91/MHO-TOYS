009c8eda04d617320ed0615402b5fe98df15d8b8
Mouayad92 <mouayad.omar@timebuzzer.com>
2025-06-27 23:08:47 +0200

    update

---
+++ backend/src/main/java/com/mho_toys/backend/BackendApplication.java
@@ -0,0 +1,18 @@
+package com.mho_toys.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+
+		System.out.println("**********************************");
+		System.out.println(" Application started successfully ");
+		System.out.println("**********************************");
+	}
+
+
+}
