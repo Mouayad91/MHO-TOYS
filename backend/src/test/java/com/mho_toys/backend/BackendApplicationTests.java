3068dfa21c283d6879510a60974156c4a863b2b0
Mouayad92 <mouayad.omar@timebuzzer.com>
2025-06-27 23:39:46 +0200

    Testing ci/cd

--- backend/src/test/java/com/mho_toys/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/mho_toys/backend/BackendApplicationTests.java
@@ -2,12 +2,15 @@ package com.mho_toys.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// This test will pass if the application context loads successfully
 	}
 
 }
