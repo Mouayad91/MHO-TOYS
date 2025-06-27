1b1e1e0469e3305d80bf35f4bb4ca511e6b40cc9
Mouayad92 <mouayad.omar@timebuzzer.com>
2025-06-27 23:47:51 +0200

    model mapper for later dtos and entities

--- backend/src/main/java/com/mho_toys/backend/config/ModelMapperConfig.java
+++ backend/src/main/java/com/mho_toys/backend/config/ModelMapperConfig.java
@@ -1,4 +1,22 @@
 package com.mho_toys.backend.config;
 
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ModelMapperConfig {
+
+    /**
+     * Bean configuration for ModelMapper.
+     * This allows for automatic mapping between DTOs and entities.
+     *
+     * @return a new instance of ModelMapper
+     */
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
